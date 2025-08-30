6354a8acebed58d6df295b40ee605bc64cb1e806
rawanyasser9 <rawannyasser2004@gmail.com>
2025-08-30 23:10:39 +0300

    Add files via upload

---
+++ C#/calcTask/calcTask/Calc.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calcTask
+{
+    public static class CalcExtensions
+    {
+        public static double Add(this double x, double y) => x + y;
+        public static double Subtract(this double x, double y) => x - y;
+        public static double Multiply(this double x, double y) => x * y;
+        public static double Divide(this double x, double y)
+        {
+            if (y == 0)
+            {
+                Console.WriteLine("can not divide by zero");
+                return 0;
+            }
+            return x / y;
+        }
+
+
+        public static int Add(this int x, int y) => x + y;
+        public static int Subtract(this int x, int y) => x - y;
+        public static int Multiply(this int x, int y) => x * y;
+        public static int Divide(this int x, int y)
+        {
+            if (y == 0)
+            {
+                Console.WriteLine("can not divide by zero");
+                return 0;
+            }
+            return x / y;
+        }
+
+    }
+
+    
+}
---
+++ C#/calcTask/calcTask/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace calcTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            double y = 10.5;
+
+            Console.WriteLine(x.Add(5));           
+            Console.WriteLine(x.Subtract(3));     
+            Console.WriteLine(x.Multiply(4));        
+            Console.WriteLine(x.Divide(2));          
+     
+            Console.WriteLine(y.Add(2.5));          
+            Console.WriteLine(y.Divide(2.5));        
+          
+            var result = x.Add(5).Multiply(2).Subtract(3);
+            Console.WriteLine(result);              
+
+
+        }
+    }
+}
